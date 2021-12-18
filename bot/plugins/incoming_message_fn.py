708bfd25ff912f8f481d126e495bf53e9b77e281
farshidband3 <82159763+farshidband3@users.noreply.github.com>
2021-12-18 12:07:53 +0330

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -165,7 +165,7 @@ async def incoming_compress_message_f(bot, update):
                 try:
                     await bot.send_message(
                         chat_id=update.chat.id,
-                        text="<b>⁦⚙️⁩ لطفا از بین عدد های 10 تا 90 انتخاب کنید.</b>",
+                        text="<b>⁦ ⚙️⁩ لطفا از بین عدد های 10 تا 90 انتخاب کنید.</b> ",
                         reply_to_message_id=update.message_id
                     )
                     return
